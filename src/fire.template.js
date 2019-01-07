4674897ad4a3b183129e1528fccb984396821a89
unknown <GH36551X@NGN10776707037.cimbniaga.co.id>
2019-01-07 18:17:04 +0700

    delete firebase auth

---
+++ src/fire.template.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase'
+
+const config = {
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: ""
+}
+
+const fire = firebase.initializeApp(config);
+
+export default fire;
